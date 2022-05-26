dfba6bfa229ad5676a135e7ce036c9f1d501570c
Jason Bell <jbell@accelebrate.com>
2022-05-26 14:17:58 -0700

    Update

---
+++ FunctionalDemo/FunctionalDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using FunctionalDemo;
+
+var nums = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+var evens = nums.Where(i => i % 2 == 0);
+
+foreach (var n in evens) {
+    Console.WriteLine(n);
+}
+
+Console.WriteLine("------------");
+
+nums.Add(12);
+
+foreach (var n in evens) {
+    Console.WriteLine(n);
+}
