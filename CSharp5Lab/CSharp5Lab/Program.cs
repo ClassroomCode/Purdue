7beaecb7bc3f12631c4db7b88fce2b07131bf2f4
Jason Bell <jbell@accelebrate.com>
2022-05-26 09:48:59 -0700

    P1

---
+++ CSharp5Lab/CSharp5Lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp5Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CSharpLab/CSharpLab/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int a;
+int b;
+
+Console.Write("Num1: ");
+a = int.Parse(Console.ReadLine());
+
+Console.Write("Num2: ");
+b = int.Parse(Console.ReadLine());
+
+var sum = a + b;
+
+Console.WriteLine("-----");
+
+Console.WriteLine("Sum: " + sum);
+
