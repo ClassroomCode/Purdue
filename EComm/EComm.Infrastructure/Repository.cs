1e9f79cf60cff1291b178f4b320cee36c9373d76
Jason Bell <jbell@accelebrate.com>
2022-05-27 13:45:28 -0700

    DB

---
+++ EComm/EComm.Infrastructure/Repository.cs
@@ -0,0 +1,15 @@
+﻿using EComm.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace EComm.Infrastructure;
+
+public class Repository : DbContext
+{
+    public DbSet<Supplier> Suppliers => Set<Supplier>();
+    public DbSet<Product> Products => Set<Product>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EComm;Integrated Security=True");
+    }
+}
