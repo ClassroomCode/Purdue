2c9f74666e075826cc9211af885fe4d36158e71a
Jason Bell <jbell@accelebrate.com>
2022-05-27 13:25:43 -0700

    Ext

---
+++ ExtMethodDemo/ExtMethodDemo/MyExt.cs
@@ -0,0 +1,11 @@
+﻿namespace ExtMethodDemo;
+
+public static class MyExt
+{
+    public static int DoubleIt(this int a) => a * 2;
+
+    public static string GetAssembly(this object obj)
+    {
+        return (obj.GetType().Assembly.GetName().Name ?? "Unknown");
+    }
+}
