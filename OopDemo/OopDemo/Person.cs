d321f4ce99d76d0b140ef391017a662d513cdbbf
Jason Bell <jbell@accelebrate.com>
2022-05-27 07:50:17 -0700

    Update

---
+++ OopDemo/OopDemo/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace OopDemo;
+
+public class Person
+{
+    public string FirstName = string.Empty;
+    public string LastName = string.Empty;
+
+    public string FullName()
+    {
+        return $"{FirstName} {LastName}";
+    }
+}
---
+++ OopDemo/OopDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using OopDemo;
+
+var people = new List<Person>();
+
+var p1 = new Person();
+p1.FirstName = "Bill";
+p1.LastName = "Gates";
+people.Add(p1);
+
+var p2 = new Person();
+p2.FirstName = "Steve";
+p2.LastName = "Jobs";
+people.Add(p2);
+
+foreach (var p in people) {
+    Console.WriteLine(p.FullName());
+}
+
