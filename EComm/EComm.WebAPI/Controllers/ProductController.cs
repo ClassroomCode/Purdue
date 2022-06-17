923be832f4a755c103951156b7e0a6381a351720
Jason Bell <jbell@accelebrate.com>
2022-06-17 09:05:35 -0700

    API part one

---
+++ EComm/EComm.WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EComm.WebAPI.Controllers
+{
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IRepository _repository;
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(IRepository repository,
+            ILogger<ProductController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        
+    }
+}
