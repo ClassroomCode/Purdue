63f531115e76f70ec8a6ddc6258c8265f217e014
Jason Bell <jbell@accelebrate.com>
2022-06-17 11:42:52 -0700

    Prog Auth

--- EComm/EComm.WebApp/Program.cs
+++ EComm/EComm.WebApp/Program.cs
@@ -1,11 +1,22 @@
 global using EComm.Core;
 using EComm.Infrastructure;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IRepository, Repository>();
+
+builder.Services.AddAuthentication(
+  CookieAuthenticationDefaults.AuthenticationScheme)
+  .AddCookie(options => { options.LoginPath = "/login"; });
+
+builder.Services.AddAuthorization(options => {
+    options.AddPolicy("AdminsOnly", policy =>
+      policy.RequireClaim(ClaimTypes.Role, "Admin"));
+});
 
 var app = builder.Build();
 
@@ -21,6 +32,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();  // sets the User object
 app.UseAuthorization();
 
 app.MapControllers();
