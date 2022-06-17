fd926468a9370cdcabdc58e759211bcc211dc71c
Jason Bell <jbell@accelebrate.com>
2022-06-17 07:36:13 -0700

    Val

--- EComm/EComm.WebApp/Models/ProductEditViewModel.cs
+++ EComm/EComm.WebApp/Models/ProductEditViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace EComm.WebApp.Models;
 
@@ -6,8 +7,13 @@ public class ProductEditViewModel
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage ="Product must have a name!")]
     public string ProductName { get; set; } = string.Empty;
+
+    [Required]
+    [Range(1, 500)]
     public Decimal? UnitPrice { get; set; }
+  
     public string? Package { get; set; }
     public bool IsDiscontinued { get; set; }
 
